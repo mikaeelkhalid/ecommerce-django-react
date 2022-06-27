4aecd15c899dea01213018f2a610f496181a2d83
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2022-06-28 03:12:45 +0500

    adding products api

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for p in products:
+        if p['_id'] == pk:
+            product = p
+            break
+        else:
+            product = "Product not found"
+    return Response(product)
